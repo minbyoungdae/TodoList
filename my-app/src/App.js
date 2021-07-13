1b2287d002bda832694e1b697a672de5ee1488c2
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2021-07-13 19:04:42 +0900

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <h1>안녕하세요</h1>
+  );
+}
+
+export default App;
